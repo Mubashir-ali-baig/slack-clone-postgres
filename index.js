2f3461489370acc82079aad0b9ab3ce597b8c3a6
mubashir98 <mubashirbaig98@gmail.com>
2019-07-10 15:35:30 +0500

    1st nodejs modification

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress,graphiqlExpress } from 'graphql-server-express';
+import {makeExecutableSchema} from 'graphql-tools';
+
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import models from './models/index';
+
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+
+const graphqlEndpoint='/graphql';
+
+app.use('/graphiql', 
+graphiqlExpress({endpointURL: graphqlEndpoint}));
+
+app.use('/graphql', bodyParser.json(), 
+graphqlExpress({schema}));
+
+
+
+models.sequelize.sync().then(()=>{
+    app.listen(8081);
+})
